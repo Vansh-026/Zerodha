484692707494c2183761765c7ebb2be3b156681e
Vansh Hinge <vanshhinge661@gmail.com>
2025-07-20 18:20:11 +0530

    final changes

--- backend/index.js
+++ backend/index.js
@@ -30,16 +30,6 @@ app.use(
     credentials: true,
   })
 );
-app.options("*", cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
 
 
 // Middleware
